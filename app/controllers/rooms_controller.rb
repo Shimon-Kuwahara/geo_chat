7a1b2909b930124dde057c7e3ac7ae4b9777f267
mean <aenma2004@gmail.com>
2024-06-19 14:21:18 +0900

    チャット一覧できた！

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,11 @@
 class RoomsController < ApplicationController
-    def show
-        @rooms = current_user.user_rooms
+  before_action :authenticate_user!
+
+  def show
+    if current_user.nil?
+      redirect_to new_user_session_path, alert: "You need to sign in or sign up before continuing."
+    else
+      @rooms = UserRoom.where(user_id: current_user.id) # Adjusted to user_id if necessary
     end
+  end
 end
