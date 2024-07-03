5a79e1c8163dbf0250cd734010090e36485528ba
Shimon-Kuwahara <tukubamj@icloud.com>
2024-07-03 13:35:35 +0900

    update location

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
   get '/myrooms', to: 'rooms#show'
+  post 'users/update', to: 'users#update_location'
+  post 'users/init', to: 'users#init_location'
 
   resources :chats, only: %i[show create]
   resources :users
