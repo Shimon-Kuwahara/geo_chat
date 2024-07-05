b8a54885069707acc34e9a9b4c04355b47fe2986
Shimon-Kuwahara <tukubamj@icloud.com>
2024-07-05 16:44:50 +0900

    マッチ距離の指定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_12_034945) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_05_071558) do
   create_table "chats", force: :cascade do |t|
     t.integer "user_id"
     t.integer "room_id"
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_12_034945) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "match_distance", default: 50, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
