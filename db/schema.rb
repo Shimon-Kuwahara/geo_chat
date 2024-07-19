83268bfc83ecdbff32cef7c4359edfad963a6bd5
Hinano Kawakami <ttysh66482ten@gmail.com>
2024-07-19 17:19:57 +0900

    game design

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_07_05_071558) do
-
   create_table "chats", force: :cascade do |t|
     t.integer "user_id"
     t.integer "room_id"
