42b9f6991ab703415c8278ef51b36c7b4b82f5e7
Shimon-Kuwahara <tukubamj@icloud.com>
2024-07-24 12:45:12 +0900

    チャット送信の改善

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,6 @@ set :environment, rails_env # cronを実行する環境変数をセット
 ENV.each { |k, v| env(k, v) }
 set :output, "#{Rails.root}/log/development.log" # cronのログ出力用ファイル
 
-every 10.hour do
+every 1.minutes do
   rake 'create_matches:create'
 end
