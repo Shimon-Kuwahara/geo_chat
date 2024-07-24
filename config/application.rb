d67d7033cef1dcbab56f1612bbb66b9100a840d1
Shimon-Kuwahara <tukubamj@icloud.com>
2024-07-24 15:52:51 +0900

    時刻の日本設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module GeoChat
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.active_record.default_timezone = :local
 
     # Configuration for the application, engines, and railties goes here.
     #
